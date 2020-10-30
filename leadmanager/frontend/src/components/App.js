567d0765e657e0e65bf73edac17473b179e725a0
Maimuna Begum Kali <1810.kali@gmail.com>
2020-10-30 03:09:17 -0400

    added landing page initally

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Search from './search'
 
 class App extends Component {
     render() {
         return (
-            <h1>Hello World</h1>
+           <Search />
         )
     }
 }
