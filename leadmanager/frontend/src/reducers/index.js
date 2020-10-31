36ed8d4da7bc97becfe81132b2a1490dd4536008
Bryan Consuegra <49498666+bcons009@users.noreply.github.com>
2020-10-31 17:50:19 -0400

    Merged index.js
    trevor + bryan branches

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -2,9 +2,15 @@ import { combineReducers } from 'redux'
 import auth from './auth'
 import errors from './errors'
 import messages from './messages'
+import locations from './locations'
+import schedules from './schedules'
+import locationsLL from './locationsLL'
 
 export default combineReducers({
 	auth,
 	errors,
-	messages
+	messages,
+	locations, 
+        schedules,
+        locationsLL
 });
