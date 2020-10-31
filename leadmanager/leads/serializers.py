f9d30f4c5b088294d148196bde39b7984318930a
Bryan Consuegra <49498666+bcons009@users.noreply.github.com>
2020-10-31 17:06:42 -0400

    Update serializers.py
    Updated with data from bryan branch

--- leadmanager/leads/serializers.py
+++ leadmanager/leads/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from leads.models import Lead
+from leads.models import OrgLocation, OrgSchedule, OrgLocationLL
 
-# Lead Serializer
-class LeadSerializer(serializers.ModelSerializer):
+# OrgLocation Serializer
+class OrgLocationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Lead
+        model = OrgLocation
+        fields = '__all__'
+
+# OrgSchedule Serializer
+class OrgScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrgSchedule
+        fields = '__all__'
+
+# OrgLocationLL Serializer
+class OrgLocationLLSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrgLocationLL
         fields = '__all__'
