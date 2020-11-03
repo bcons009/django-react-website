4df8e6e184f8bf645b157a1d1ad1676724eabfcb
sabna <sabnaabdulrahman@gmail.com>
2020-11-02 20:55:46 -0500

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,20 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
-            }
+            },
+            {
+                test: /\.css$/i,
+                exclude: /node_modules/,
+                use: [
+                  'style-loader',
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      modules: true,
+                    },
+                  },
+                ],
+              },
         ]
     }
 }
