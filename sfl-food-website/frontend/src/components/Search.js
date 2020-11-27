dfd2e58844691eb3d9d4ad733fb620d8830a352d
sabna <sabnaabdulrahman@gmail.com>
2020-11-27 15:40:13 -0500

    Add files via upload

--- sfl-food-website/frontend/src/components/Search.js
+++ sfl-food-website/frontend/src/components/Search.js
@@ -1,108 +1,104 @@
-import React, { Component } from "react";
-// import ReactDOM from "react-dom";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
 import styles from "../mystyle.module.css";
+import PropTypes from "prop-types";
+import { getLocationsLL } from '../actions/locationsLL'
 import MapDisplay from "./displays/MapDisplay";
-import Informationpage from './Informationpage.js';
+//import { getSchedules } from '../actions/schedules'
 import { Link } from "react-router-dom";
+import LinkButton from "./misc/LinkButton";
 
-export default class Search extends Component {
+export class Search extends Component {
+  static propTypes = {
+    locationsLL: PropTypes.array.isRequired,
+  };
+  state = {
+    searchValue: "",
+    //meals: []
+  };
 
-    state = {
-        searchValue: "",
-        meals: []
-      };
-    
-      handleOnChange = event => {
-        this.setState({ searchValue: event.target.value });
-      };
-    
-      handleSearch = () => {
-        this.makeApiCall(this.state.searchValue);
-      };
-    
-      makeApiCall = searchInput => {
-        var searchUrl = `http://localhost:8000/api/locationsLL/`;
-        /*fetch(searchUrl)
-          .then(response => {
-           
-            //alert(JSON.stringify(response)) ;
-            var jsondata = "{\"meals\": [{\"eventid\": \"52955\",\"eventName\": \"Free give away\",\"eventAddress\": \"123 Vermont St, Weston, FL\",\"eventDate\": \"12/10/2020\"},{\"eventid\": \"52355\",\"eventName\": \"YMCA Food Drive\",\"eventAddress\": \"1464 Nashville St, Miami, FL\",\"eventDate\": \"11/21/2020\"},{\"eventid\": \"555\",\"eventName\": \"Publix Food Drive\",\"eventAddress\": \"21 Baker St, Cooper city, FL\",\"eventDate\": \"10/29/2020\"}]}";
-            document.getElementById("divmap").style.visibility = "visible";
-            return JSON.parse(jsondata);
-          })
-          .then(jsonData => {
-           // alert("meaks"+meals);
-            console.log("OK",JSON.stringify(jsonData.meals));
-            this.setState({ meals: jsonData.meals });
-          });*/
-          let currentComponent = this;
-          fetch(searchUrl).then(function(response) {
-            response.json().then(function(parsedJson) {
-            document.getElementById("divmap").style.visibility = "visible";
-            console.log("parsedJson",parsedJson);
-            return parsedJson;
-              
-            })
-            .then(jsonData  => {
-              currentComponent.setState({ meals: jsonData  });
-            });
-          })
-         
-      };
+  handleOnChange = (event) => {
+    this.setState({ searchValue: event.target.value });
+  };
+
+  handleSearch = () => {
+    console.log("button click","")
+    this.props.getLocationsLL();
+    //document.getElementById("divmap").style.visibility = "visible";
+
+  };
+  
+
   render() {
     return (
-      <div className={styles.main} >
-        <div className={styles.searchContainer}>
-          <h1>South Florida Free Food</h1>
-          <input name="text" type="text" onChange={event => this.handleOnChange(event)} value={this.state.searchValue} placeholder="Search" />
-          <button onClick={this.handleSearch}>Search</button>
-          
-        </div>
-        {this.state.meals ? (
-        <div className={styles.mealsContainer}>
-          <div id="divmap" className={styles.mapSection}>
-            {/*
-              <img
-                width="90%"
-                src="https://www.themealdb.com/images/media/meals/58oia61564916529.jpg"
-                alt="meal-thumbnail"
-              />
-            */}
-            <MapDisplay />
+      <Fragment>
+        <div className={styles.main}>
+          <div className={styles.searchContainer}>
+            <h1>South Florida Free Food</h1>
+            <input
+              name="text"
+              type="text"
+              onChange={(event) => this.handleOnChange(event)}
+              value={this.state.searchValue}
+              placeholder="Search"
+            />
+            <button onClick={this.handleSearch}>Search</button>
           </div>
-          {this.state.meals.filter(meals =>meals.name.toLowerCase().includes(this.state.searchValue.toLowerCase()) || meals.description.toLowerCase().includes(this.state.searchValue.toLowerCase())).map((meal, index) => (
-          <div className={styles.singleMeal} key={index}>
-            <div className={styles.singleMealContainer}>
-              <div className={styles.singleMealLeft}>
-                <h2>
+          {this.props.locationsLL ? (
+            <div className={styles.mealsContainer}>
+              <div id="divmap" className={styles.mapSection}>
+                
+                <MapDisplay />
+              </div>
+              {this.props.locationsLL.filter(locationsLL =>locationsLL.name.toLowerCase().includes(this.state.searchValue.toLowerCase()) || locationsLL.description.toLowerCase().includes(this.state.searchValue.toLowerCase())).map((location, index) => (
+              <div className={styles.singleMeal} key={index}>
+                <div className={styles.singleMealContainer}>
+                  <div className={styles.singleMealLeft}>
+                    <h2>
+                   
+                      <Link to={`/Informationpage/${location.id}`}>{location.name}</Link>
+                    </h2>
+                    { location.description.length<300 ?
+                    (<p>
+                      {location.description}
+                    </p>)
+                    : (<p>{location.description.substring(0, 300)}"..." <Link to={`/Informationpage/${location.id}`}>More</Link></p>)}
+                    <p>
+                      <b>Serving </b>: {location.cost}
+                    </p>
+                  </div>
+                  <div className={styles.singleMealRight}>
+                    <p>{location.email}</p>
+                    <p>{location.phone_number}</p>
+                    <a href="#">{location.address}</a>
+                    <p><LinkButton
+                    
+                    to={{
+                        pathname: `/Informationpage/${location.id}`,
+                        id: location.id,
+                    }}
+                >
+                    Edit Listing
+                </LinkButton></p>
+                  </div>
+                </div>
                
-                  <Link to={`/Informationpage/${meal.id}/`}>{meal.name}</Link>
-                </h2>
-                { meal.description.length<300 ?
-                (<p>
-                  {meal.description}
-                </p>)
-                : (<p>{meal.description.substring(0, 300)}"..." <Link to={`/Informationpage/${meal.id}/`}>More</Link></p>)}
-                <p>
-                  <b>Serving </b>: {meal.cost}
-                </p>
               </div>
-              <div className={styles.singleMealRight}>
-                <p>{meal.email}</p>
-                <p>{meal.phone_number}</p>
-                <a href="#">{meal.address}</a>
-              </div>
+              ))}
             </div>
-           
-          </div>
-          ))}
+            ) : (
+                <p>Try searching for a event</p>
+              )}
         </div>
-        ) : (
-            <p>Try searching for a meal</p>
-          )}
-      </div>
+      </Fragment>
     );
   }
 }
 
-// ReactDOM.render(<Search />, document.getElementById("app"));
+const mapStateToProps = (state) => ({
+  locationsLL: state.locationsLL.locationsLL
+  // 1st locations = reducer
+  //schedules: state.schedules.locations
+});
+
+export default connect(mapStateToProps, { getLocationsLL })(Search);
