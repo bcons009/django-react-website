39eaf22b38fe6c40572f72c4c88f974fbe55f34d
Maimuna Begum Kali <maimuna_kali@yahoo.com>
2020-11-27 15:37:23 -0500

    Add files via upload

---
+++ AddReview.js
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from 'react'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import AddReviewForm from './AddReviewForm'
+import UserAccounts from './UserAccounts'
+import { Provider } from 'react-redux';
+import store from '../store'
+
+import Header from './layout/Header';
+
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+
+export default class AddReview extends Component {
+    render() {
+        return (
+            <div>
+                <Provider store={ store }>
+                    <Router>
+                        <Fragment>
+                            <Header />
+                            <Switch>
+                                <Route exact path="/login" component={ Login } />
+                                <Route exact path="/register" component={ Register } />
+                            </Switch>
+                        </Fragment>
+                        <AddReviewForm />
+                    </Router> 
+                </Provider>               
+            </div>
+        )
+    }
+}
